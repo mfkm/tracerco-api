eb7559906603f6506bec7e09f2a8d9d9a969fa35
Muhammad Firdhaus Khairil Mokhtar <firdhauskm@gmail.com>
2022-11-16 21:38:28 +0800

    added APIs for simple calculator

---
+++ Controllers/SimpleCalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tracerco_api.Interfaces;
+
+namespace tracerco_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleCalculatorController : ControllerBase
+    {
+        private readonly ISimpleCalculator _calculatorService;
+
+        [HttpGet]
+        [Route("Add")]
+        public int Add(int start, int amount)
+        {
+            return _calculatorService.Add(start, amount);
+        }
+
+        [HttpGet]
+        [Route("Divide")]
+        public int Divide(int start, int by)
+        {
+            return _calculatorService.Divide(start, by);
+        }
+
+        [HttpGet]
+        [Route("Multiply")]
+        public int Multiply(int start, int by)
+        {
+            return _calculatorService.Multiply(start, by);
+        }
+
+        [HttpGet]
+        [Route("Subtract")]
+        public int Subtract(int start, int amount)
+        {
+            return _calculatorService.Subtract(start, amount);
+        }
+
+    }
+}
---
+++ Services/SimpleCalculatorService.cs
@@ -0,0 +1,27 @@
+﻿using tracerco_api.Interfaces;
+
+namespace tracerco_api.Services
+{
+    public class SimpleCalculatorService : ISimpleCalculator
+    {
+        public int Add(int start, int amount)
+        {
+            return start + amount;
+        }
+
+        public int Divide(int start, int by)
+        {
+            return start / by;
+        }
+
+        public int Multiply(int start, int by)
+        {
+            return start * by;
+        }
+
+        public int Subtract(int start, int amount)
+        {
+            return start - amount;
+        }
+    }
+}
